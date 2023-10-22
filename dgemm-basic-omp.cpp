0ffd3c2c40d3df0a0ffbebddbd61629fdb230664
BENZ buddharuksa <ebuddharuksa@sfsu.edu>
2023-10-22 16:01:35 -0700

    finished code in pc

--- dgemm-basic-omp.cpp
+++ dgemm-basic-omp.cpp
@@ -14,26 +14,16 @@ void square_dgemm(int n, double *A, double *B, double *C)
 {
 #pragma omp parallel
    {
-#ifdef LIKWID_PERFMON
       LIKWID_MARKER_START(MY_MARKER_REGION_NAME);
-#endif
-
 #pragma omp for collapse(2)
       for (int i = 0; i < n; ++i)
-      {
          for (int j = 0; j < n; ++j)
          {
-            double cij = C[i + j*n];
             for (int k = 0; k < n; ++k)
             {
-               cij += A[i + k * n] * B[k + j * n]; // A and B are in column-major format
+               C[i + j * n] += A[i + k * n] * B[k + j * n];
             }
-            C[i + j*n] = cij;
          }
-      }
-
-#ifdef LIKWID_PERFMON
       LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME);
-#endif
    }
 }
